580938c167cc242e60d245b8cbacc5f7bf4c12c2
mmohashik <127235049+mmohashik@users.noreply.github.com>
2025-03-22 05:14:03 +0530

    Pages Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vm] md:px-[7vm] lg:px-[9vw]' >
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+
+      </Routes>
     </div>
   )
 }
