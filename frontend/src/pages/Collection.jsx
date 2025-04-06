75dbcc3c315e8bfc0b3879cfc50f4fef34a7c991
mmohashik <127235049+mmohashik@users.noreply.github.com>
2025-04-06 10:31:20 +0530

    filter created
    with mobile responsive

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { ShopContext } from '../context/ShopContext'; // Make sure the path is correct
+import { assets } from '../assets/frontend_assets/assets';
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
@@ -14,10 +15,18 @@ const Collection = () => {
           onClick={() => setShowFilter(!showFilter)}
         >
           FILTERS
+          <img 
+          className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} 
+          src={assets.dropdown_icon} 
+          alt="" 
+          />
         </p>
 
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
-          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES
+          
+
+          </p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
               <input className='w-3' type='checkbox' value={'Men'}/> Men
@@ -32,6 +41,26 @@ const Collection = () => {
             </p>
           </div>
         </div>
+
+        <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type='checkbox' value={'Topwear'}/> Topwear
+            </p>
+
+            <p className='flex gap-2'>
+              <input className='w-3' type='checkbox' value={'Bottomwear'}/> Bottomwear
+            </p>
+
+            <p className='flex gap-2'>
+              <input className='w-3' type='checkbox' value={'Winterwear'}/> Winterwear
+            </p>
+          </div>
+        </div>
+
+
+
       </div>
     </div>
   );
