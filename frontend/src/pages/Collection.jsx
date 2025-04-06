01e7b2b302f5a1b272fad7e52ee34ba094bbb49e
mmohashik <127235049+mmohashik@users.noreply.github.com>
2025-04-06 13:24:58 +0530

    int

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { ShopContext } from '../context/ShopContext'; // Make sure the path is correct
 import { assets } from '../assets/frontend_assets/assets';
+import Title from '../components/Title';
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const []
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -59,9 +61,31 @@ const Collection = () => {
           </div>
         </div>
 
+      </div>
+
+      <div className='flex-1'>
+
+      <div className='flex justify-between text-base sm:text-2xl mb-4'>
+        <Title text1={'ALL'} text2={'COLLECTIONS'}/>
+
+        <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavnet">Sort by : Relavant</option>
+            <option value="low-high">Sort by : Low to High</option>
+            <option value="high-low">Sort by : High to Low</option>
+        </select>
+      </div>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
 
 
       </div>
+
+
+      </div>
+
+
+
+
     </div>
   );
 };
