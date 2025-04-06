17c5b3bc6feea6931642e0dd1a15ce9a52ec1567
mmohashik <127235049+mmohashik@users.noreply.github.com>
2025-04-06 10:10:59 +0530

    added filter comp

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext'; // Make sure the path is correct
 
 const Collection = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
 
-export default Collection
+  return (
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* Filter section */}
+      <div className='min-w-60'>
+        <p 
+          className='my-2 text-xl flex items-center cursor-pointer gap-2'
+          onClick={() => setShowFilter(!showFilter)}
+        >
+          FILTERS
+        </p>
+
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          {/* Add filter content here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collection;
